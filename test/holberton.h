bc738f07b9ae840d747b0aa7d05db5d74e17df61
tosattijames <703@holbertonschool.com>
2019-03-18 17:40:55 +0000

    wip Header

---
+++ test/holberton.h
@@ -0,0 +1,13 @@
+ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <limits.h>
+#include <stdio.h>
+
+int _putchar(int c); 
+
+/** printf */
+int _printf(const char *format, ...);
+
+
+#endif /* HOLBERTON_H */
